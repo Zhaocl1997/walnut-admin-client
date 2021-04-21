01df8ef4fecc3a92f093fdc566ee6ffeaa1b7049
zhaocl1997 <674351624@qq.com>
2021-04-21 22:42:02 +0800

    feat(appContext): support for usage of app context outside setup

--- src/App/src/hooks/useAppContext.ts
+++ src/App/src/hooks/useAppContext.ts
@@ -1,15 +1,35 @@
 import type { ToRefs } from 'vue'
 import type { AppContext } from '../types'
 
+import { getCurrentInstance } from 'vue'
+
 import { useContext } from '/@/hooks/core/useContext'
 import { appKey } from '../utils/InjectionKey'
+import { AppGlobalContext } from '/@/store/ctx'
 
 const { setContext, getContext } = useContext<ToRefs<AppContext>>()
 
+/**
+ * @description Initial App Context
+ */
 export const setAppContext = (initialState: ToRefs<AppContext>) => {
   setContext(appKey, initialState)
 }
 
-export const getAppContext = () => {
-  return getContext(appKey)
+/**
+ * @description App Context Core
+ *
+ * Use `overload` and `generic` to return different type
+ *
+ * Default `generic` is `true`, which stands for usage in `setup`
+ *
+ * Use `useAppContext<false>()` to access app context outside `setup` function
+ *
+ * @link https://stackoverflow.com/questions/66003736/how-to-let-typescript-return-correct-type-based-on-parameters
+ */
+export function useAppContext<T extends boolean = true>(): T extends true
+  ? ReturnType<typeof getContext>
+  : AppContext
+export function useAppContext() {
+  return getCurrentInstance() ? getContext(appKey) : AppGlobalContext
 }
