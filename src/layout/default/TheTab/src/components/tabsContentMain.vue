03cfef1c154217e6c3e27ca529b259e26622afa4
zhaocl1997 <674351624@qq.com>
2021-04-24 14:44:07 +0800

    style: optimise tab style

--- src/layout/default/TheTab/src/components/tabsContentMain.vue
+++ src/layout/default/TheTab/src/components/tabsContentMain.vue
@@ -4,7 +4,7 @@
       <li
         v-for="(tab, index) in getTabs"
         :class="[
-          'flex space-x-1 flex-nowrap bg-primary bg-opacity-100 w-auto border border-solid border-blue-500 rounded-md shadow-lg mx-0.5 p-0.5 px-2 my-1 select-none cursor-pointer',
+          'flex flex-nowrap items-center space-x-1 bg-primary bg-opacity-100 w-auto border border-solid border-blue-500 rounded-md shadow mx-0.5 px-2 my-1 select-none cursor-pointer',
           {
             'text-blue-700 bg-secondary': $route.name === tab.name,
             'hover:text-blue-600 hover:bg-secondary': $route.name !== tab.name,
@@ -14,24 +14,23 @@
         @contextmenu.prevent.native="onCtxMenu($event, tab.name, index)"
       >
         <!-- active symbol -->
-        <div v-show="$route.name === tab.name" class="inline">
-          <div
-            class="border-8 border-solid border-blue-500 rounded-full h-4 w-4 mt-0.5 hover:border-blue-700"
-          ></div>
-        </div>
+
+        <div
+          v-show="$route.name === tab.name"
+          class="border-8 border-solid border-blue-500 rounded-full h-4 w-4 hover:border-blue-700"
+        ></div>
 
         <!-- title -->
-        <div class="inline">
-          <span
-            class="whitespace-nowrap font-sans text-sm antialiased text-primary"
-            >{{ getMaybeI18nMsg(tab.meta.title) }}</span
-          >
-        </div>
+
+        <span class="whitespace-nowrap font-sans text-sm text-primary">{{
+          getMaybeI18nMsg(tab.meta.title)
+        }}</span>
 
         <!-- close icon -->
-        <div v-show="tab.name !== indexName" class="inline">
+        <div>
           <i
-            class="el-icon-close hover:bg-orange-400 rounded-full transform hover:scale-110 hover:opacity-80"
+            v-show="tab.name !== indexName"
+            class="el-icon-close text-primary hover:bg-orange-400 rounded-full transform hover:scale-110 hover:opacity-80"
             @click.prevent.stop="onTabRemove(tab.name)"
           ></i>
         </div>
