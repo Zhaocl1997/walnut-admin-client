4cc21b2333f7369548e8dda7011c5512a3814aca
zhaocl1997 <674351624@qq.com>
2021-04-27 16:48:19 +0800

    fix: fix buttonGroup type error

--- src/components/UI/ButtonGroup/src/types/index.ts
+++ src/components/UI/ButtonGroup/src/types/index.ts
@@ -2,5 +2,5 @@ import type { WButtonProps } from '/@/components/UI/Button'
 import type { SharedFormComponentEvent } from '/@/components/UI/Form'
 
 export interface WButtonGroupProps {
-  groups: WButtonProps & Partial<SharedFormComponentEvent>[]
+  groups: (WButtonProps & Partial<SharedFormComponentEvent>)[]
 }
