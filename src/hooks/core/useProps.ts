049e5ff47d71e03c459d14fe7626b9c168f02278
zhaocl1997 <674351624@qq.com>
2021-04-27 16:48:43 +0800

    feat: add useProps hook

---
+++ src/hooks/core/useProps.ts
@@ -0,0 +1,25 @@
+import { ref, unref, computed } from 'vue'
+import { easyDeepMerge } from 'easy-fns-ts'
+
+export const useProps = <T>(props: T) => {
+  const propsRef = ref<T>()
+
+  /**
+   * @description set props exposed as a method
+   */
+  const setProps = (newProps: T) => {
+    propsRef.value = easyDeepMerge(unref(propsRef), newProps)
+  }
+
+  /**
+   * @description get form props
+   */
+  const getProps = computed<T>(() => {
+    return { ...props, ...unref(propsRef) }
+  })
+
+  return {
+    setProps,
+    getProps,
+  }
+}
