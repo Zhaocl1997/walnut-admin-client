f605e80e733301715634abf672b5232c799d3294
zhaocl1997 <674351624@qq.com>
2021-04-29 21:53:42 +0800

    types: add MaybeRefRecord type

--- types/utils.d.ts
+++ types/utils.d.ts
@@ -1,10 +1,13 @@
 import type { Ref, ComputedRef } from 'vue'
 
 /**
- * Maybe it's a ref, or not.
- *
- * ```ts
- * type MaybeRef<T> = T | Ref<T> | ComputedRef<T>
- * ```
+ * @description Maybe it's a ref, or not.
  */
 declare type MaybeRef<T> = T | Ref<T> | ComputedRef<T>
+
+/**
+ * @description Maybe it's an object
+ */
+declare type MaybeRefRecord<T> = {
+  [P in keyof T]: MaybeRef<T[P]>
+}
