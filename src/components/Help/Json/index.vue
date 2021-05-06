407f76d24ab157938d45d4bb7984f0282fa90767
zhaocl1997 <674351624@qq.com>
2021-05-06 16:56:19 +0800

    refactor: refactor json component

--- src/components/Help/Json/index.vue
+++ src/components/Help/Json/index.vue
@@ -1,11 +1,18 @@
 <template>
-  <pre :id="id" class="w-json-pre" :style="{ height: height, width: width }" />
+  <pre :id="id" :style="getStyle" class="w-json-pre" />
+  <el-backtop
+    target=".w-json-pre"
+    :style="{ position: 'relative' }"
+    :right="-widthRef + 60"
+    :bottom="60"
+  />
 </template>
 
 <script lang="ts">
-  import { genString } from 'easy-fns-ts'
   import type { PropType } from 'vue'
-  import { defineComponent, watch, onMounted, nextTick } from 'vue'
+  import { genString } from 'easy-fns-ts'
+  import { defineComponent, computed, ref, watchEffect } from 'vue'
+  import { highlightSyntax } from './utils'
 
   export default defineComponent({
     name: 'WJSon',
@@ -27,81 +34,50 @@
         type: String as PropType<string>,
         default: '100%',
       },
+
+      resizable: {
+        type: Boolean as PropType<boolean>,
+        default: false,
+      },
     },
 
     setup(props) {
       const id = genString(8)
+      const widthRef = ref(0)
 
-      watch(
-        () => props.value,
-        () => {
-          init()
-        },
-        {
-          deep: true,
+      const getStyle = computed((): any => {
+        return {
+          height: props.height,
+          width: props.width,
+          resize: props.resizable ? 'both' : 'none',
         }
-      )
-
-      const onSyntaxHighlight = (json: string) => {
-        if (!json) {
-          return
-        }
-
-        if (typeof json != 'string') {
-          json = JSON.stringify(
-            json,
-            (key, value) => {
-              return typeof value === 'function' ? value + '' : value
-            },
-            2
-          )
-        }
-
-        json = json.replace(/&/g, '&').replace(/</g, '<').replace(/>/g, '>')
-
-        return json.replace(
-          /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null|function|=>)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
-          (match) => {
-            let cls = 'number'
-            if (/function|=>/.test(match)) {
-              cls = 'function'
-            } else if (/^"/.test(match)) {
-              if (/:$/.test(match)) {
-                cls = 'key'
-              } else {
-                cls = 'string'
-              }
-            } else if (/true|false/.test(match)) {
-              cls = 'boolean'
-            } else if (/null/.test(match)) {
-              cls = 'null'
-            }
-
-            return `<span class="${cls}">${match}</span>`
-          }
-        )
-      }
-
-      const init = () => {
-        nextTick(() => {
-          const target: HTMLElement = document.getElementById(id)!
-          const perttierJSON = onSyntaxHighlight(props.value)!
-          target.innerHTML = perttierJSON
-        })
-      }
-
-      onMounted(() => {
-        init()
       })
+
+      watchEffect(
+        () => {
+          const target: HTMLElement = document.getElementById(id)!
+          const perttierJSON = highlightSyntax(props.value)!
+          target.innerHTML = perttierJSON
+
+          widthRef.value = target.offsetWidth
+        },
+        { flush: 'post' }
+      )
 
       return {
         id,
+        getStyle,
+        widthRef,
       }
     },
   })
 </script>
 
 <style lang="scss" scoped>
+  .w-json-pre {
+    position: relative;
+  }
+
   .w-json-pre:deep() {
     padding: 5px;
     margin: 5px;
