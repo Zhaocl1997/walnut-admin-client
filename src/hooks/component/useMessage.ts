0983614d543dee4391e5f99a4cc6714de0829d1e
zhaocl1997 <674351624@qq.com>
2021-05-08 19:06:56 +0800

    feat: add useTodo message function

--- src/hooks/component/useMessage.ts
+++ src/hooks/component/useMessage.ts
@@ -2,6 +2,7 @@ import type { IMessageOptions } from 'element-plus/lib/el-message/src/types'
 import type { ElMessageBoxOptions } from 'element-plus/lib/el-message-box/src/message-box.type'
 import type { INotificationOptions } from 'element-plus/lib/el-notification/src/notification.type'
 import { ElMessage, ElMessageBox, ElNotification } from 'element-plus'
+import { useI18n } from '/@/locales'
 
 /**
  * @link https://element-plus.gitee.io/#/zh-CN/component/message#options
@@ -22,4 +23,25 @@ export const useMessageBox = (opt: ElMessageBoxOptions) => {
  */
 export const useNotification = (opt: INotificationOptions) => {
   return ElNotification(opt)
+}
+
+/**
+ * @description operations to confirm weather to continue, like delete or other important operations
+ */
+export const useTodo = (msg: string) => {
+  const { t } = useI18n()
+
+  return new Promise((res) => {
+    useMessageBox({
+      type: 'warning',
+      title: t('component.dialog.warning'),
+      message: msg,
+      cancelButtonText: t('component.dialog.cancel'),
+      confirmButtonText: t('component.dialog.confirm'),
+      showCancelButton: true,
+      closeOnClickModal: false,
+    })
+      .then(() => res(true))
+      .catch(() => res(false))
+  })
 }
