57a45fb20eeaae85cde24bdd4675048d4f629529
zhaocl1997 <674351624@qq.com>
2021-05-12 11:17:08 +0800

    fix: fix when val is empty, title render undefined

--- src/hooks/core/useTitle.ts
+++ src/hooks/core/useTitle.ts
@@ -5,6 +5,7 @@ import { getMaybeI18nMsg } from '/@/views/system/menu/utils'
  * @description reactive document title
  */
 export const useTitle = (val: string | undefined) => {
+  if (!val) return
   const title = VueUseTitle()
   title.value = `${getMaybeI18nMsg(val)} - ${import.meta.env.VITE_APP_TITLE}`
 }
