6c925dca93e49f51f04e845a96635041ff28ce17
zhaocl1997 <674351624@qq.com>
2021-05-14 15:28:08 +0800

    feat: add new app log type

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,5 +1,14 @@
 import { appName } from './constant/prefix'
 
+export const appTable = (data: unknown[]) => {
+  appInfo('Table Data')
+  console.table(data)
+}
+
+export const appInfo = (msg: unknown) => {
+  console.info(`[${appName}]`, msg)
+}
+
 export const appError = (msg: unknown) => {
   console.error(`[${appName}]`, msg)
 }
