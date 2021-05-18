15bcdca0ce73f670c84ad36f8edee69067e0b27d
zhaocl1997 <674351624@qq.com>
2021-05-18 22:42:24 +0800

    chore: handle stashes

--- src/components/UI/Table/src/utils/InjectionKey.ts
+++
@@ -1,6 +0,0 @@
-import type { InjectionKey } from 'vue'
-import { SymbolKeyEnum } from '/@/enums/symbol'
-
-export const tableKey: InjectionKey<string | symbol> = Symbol(
-  SymbolKeyEnum.TABLE_KEY
-)
