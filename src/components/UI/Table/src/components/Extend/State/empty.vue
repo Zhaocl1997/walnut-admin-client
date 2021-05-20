c884ae02bed7c82fc34b1ed7194d3fad7c7dbac5
zhaocl1997 <674351624@qq.com>
2021-05-20 20:58:28 +0800

    wip: table state

---
+++ src/components/UI/Table/src/components/Extend/State/empty.vue
@@ -0,0 +1,21 @@
+<script lang="tsx">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'WTableExtendStateEmpty',
+
+    inheritAttrs: false,
+
+    components: {},
+
+    props: {},
+
+    emits: [],
+
+    setup(props: any, ctx) {
+      return () => {
+        return <el-empty></el-empty>
+      }
+    },
+  })
+</script>
---
+++ src/components/UI/Table/src/components/Extend/State/loading.vue
@@ -0,0 +1,21 @@
+<script lang="tsx">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'WTableExtendStateLoading',
+
+    inheritAttrs: false,
+
+    components: {},
+
+    props: {},
+
+    emits: [],
+
+    setup(props: any, ctx) {
+      return () => {
+        return <div>Loading</div>
+      }
+    },
+  })
+</script>
