dfe6a1f0e822641b474a3658204516c9b56e87d1
zhaocl1997 <674351624@qq.com>
2021-06-03 17:25:40 +0800

    perf: ensure normal prop won't trigger form render

--- src/components/UI/Form/src/hooks/useFormSchemas.ts
+++ src/components/UI/Form/src/hooks/useFormSchemas.ts
@@ -1,16 +1,19 @@
 import type { WForm } from '../types'
 import type { ComputedRef } from 'vue'
-import { ref, unref, watch } from 'vue'
+
+import { ref, watchEffect } from 'vue'
 import { formatTree, genUUID, isArray } from 'easy-fns-ts'
 
-export const useFormSchemas = (props: ComputedRef<WForm.Props>) => {
+export const useFormSchemas = (
+  props: WForm.Props,
+  getProps: ComputedRef<WForm.Props>
+) => {
   const formSchemas = ref<WForm.Schema.Item[]>([])
 
-  watch(
-    () => unref(props).schemas,
-    (val) => {
-      // @ts-ignore
-      formSchemas.value = formatTree(val, {
+  watchEffect(() => {
+    const formatted = formatTree(
+      props.schemas ?? (getProps.value.schemas as any),
+      {
         format: (node) => {
           if (isArray(node.componentProp?.children)) {
             for (let i = 0; i < node.componentProp?.children.length; i++) {
@@ -23,13 +26,40 @@ export const useFormSchemas = (props: ComputedRef<WForm.Props>) => {
             uid: genUUID(),
           }
         },
-      })
-    },
-    {
-      deep: true,
-      immediate: true,
-    }
-  )
+      }
+    )
+
+    formSchemas.value = formatted
+
+    console.log('w-form rendered')
+  })
+
+  // watch(
+  //   () => props.value.schemas,
+  //   (val) => {
+  //     console.log(999)
+
+  //     // @ts-ignore
+  //     formSchemas.value = formatTree(val, {
+  //       format: (node) => {
+  //         if (isArray(node.componentProp?.children)) {
+  //           for (let i = 0; i < node.componentProp?.children.length; i++) {
+  //             node.componentProp.children[i].uid = genUUID()
+  //           }
+  //         }
+
+  //         return {
+  //           ...node,
+  //           uid: genUUID(),
+  //         }
+  //       },
+  //     })
+  //   },
+  //   {
+  //     deep: true,
+  //     immediate: true,
+  //   }
+  // )
 
   return {
     formSchemas,
