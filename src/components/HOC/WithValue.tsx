8d4398eaf5882196b13132285765892d14f46bdc
zhaocl1997 <674351624@qq.com>
2021-10-13 20:49:09 +0800

    feat: value hoc

---
+++ src/components/HOC/WithValue.tsx
@@ -0,0 +1,74 @@
+import type { PropType, ExtractPropTypes } from 'vue'
+
+import { ref, defineComponent, onMounted } from 'vue'
+
+const props = {
+  valueType: {
+    type: String as PropType<'string' | 'number'>,
+    default: 'string',
+  },
+
+  valueSeparator: {
+    type: String as PropType<string>,
+    default: '',
+  },
+}
+
+export type WithValueProp = Partial<ExtractPropTypes<typeof props>>
+
+export const WithValue = (
+  WrappedComponent: ReturnType<typeof defineComponent>
+) => {
+  return defineComponent({
+    inheritAttrs: false,
+
+    props,
+
+    emits: ['update:value'],
+
+    setup(prop, { attrs, emit }) {
+      const { value = '', multiple = false } = attrs as any
+      const v = ref()
+
+      const formateDefaultValue = (fn: Fn) => {
+        !prop.valueSeparator
+          ? (v.value = value.map((ov: any) => fn(ov)))
+          : (v.value = value
+              .split(prop.valueSeparator)
+              .map((ov: any) => fn(ov)))
+      }
+
+      onMounted(() => {
+        if (multiple === true || multiple === '') {
+          if (prop.valueType === 'string') {
+            formateDefaultValue((ov) => ov.toString())
+          } else {
+            formateDefaultValue((ov) => +ov)
+          }
+        } else {
+          if (prop.valueType === 'string') {
+            v.value = value + ''
+          } else {
+            v.value = +value
+          }
+        }
+      })
+
+      const onUpdateValue = (val: StringOrNumber[]) => {
+        v.value = val
+        emit(
+          'update:value',
+          !prop.valueSeparator ? val : val.join(prop.valueSeparator)
+        )
+      }
+
+      return () => (
+        <WrappedComponent
+          {...attrs}
+          value={v.value}
+          onUpdateValue={onUpdateValue}
+        />
+      )
+    },
+  })
+}
