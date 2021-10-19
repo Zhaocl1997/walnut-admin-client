7513c3ab17107edaafc37a08c08980dcfb6d2537
zhaocl1997 <674351624@qq.com>
2021-10-19 15:59:22 +0800

    feat: useInitialState

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,27 @@ export const emptyFunction = () => {}
 export const getApiPrefix = (base: string, version: number) => {
   return `/${base}/v${version}`
 }
+
+/**
+ * @description normalize the state usage and manegement, only work for shallow object
+ */
+export const useInitialState = <T extends Recordable>(state: T) => {
+  const getInitialState = () => ({ ...state })
+
+  const stateRef = ref<T>(getInitialState())
+
+  const setState = (newState: T) => {
+    Object.assign(stateRef.value, newState)
+  }
+
+  const resetState = () => {
+    for (const [k, v] of Object.entries(stateRef.value))
+      stateRef.value[k] = getInitialState()[k]
+  }
+
+  return {
+    stateRef,
+    setState,
+    resetState,
+  }
+}
