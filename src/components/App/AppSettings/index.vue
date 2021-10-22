2b3697eed924dad1934385740a88c78d3af06d4b
zhaocl1997 <674351624@qq.com>
2021-10-22 19:29:26 +0800

    wip: app settings

---
+++ src/components/App/AppSettings/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <w-icon
+      height="24"
+      icon="ant-design:setting-outlined"
+      @click="() => (show = true)"
+    ></w-icon>
+
+    <w-drawer v-model:show="show" :width="350" title="App Settings">
+      App Settings
+      <br />
+      App Settings
+      <br />
+      App Settings
+      <br />
+      App Settings
+      <br />
+      App Settings
+      <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings <br />
+      App Settings
+      <br />
+    </w-drawer>
+  </div>
+</template>
+
+<script lang="ts">
+  export default defineComponent({
+    name: 'AppSettings',
+  })
+</script>
+
+<script lang="ts" setup>
+  const show = ref(false)
+</script>
