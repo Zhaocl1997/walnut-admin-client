ce584839169979f4c1b5a868d4f0daeb358cc709
zhaocl1997 <674351624@qq.com>
2021-10-27 11:37:49 +0800

    refactor: iconify collection changes

--- src/components/UI/Icon/src/utils/collections.ts
+++ src/components/UI/Icon/src/utils/collections.ts
@@ -1,9 +1,9 @@
 import antdIcons from '@iconify/json/json/ant-design.json'
-import emojioneIcons from '@iconify/json/json/emojione.json'
 import mdiIcons from '@iconify/json/json/mdi.json'
 import simpleIcons from '@iconify/json/json/simple-icons.json'
+import carbonIcons from '@iconify/json/json/carbon.json'
 
 /**
  * @description Add the collection you want to use below.
  */
-export const iconCollections = [antdIcons, emojioneIcons, mdiIcons, simpleIcons]
+export const iconCollections = [antdIcons, mdiIcons, simpleIcons, carbonIcons]
