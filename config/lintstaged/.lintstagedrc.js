3b9330e158d4499810910ad1bd7e59490cbb16b1
zhaocl1997 <674351624@qq.com>
2021-11-06 11:39:23 +0800

    chore: no more stylelint for vue files

--- config/lintstaged/.lintstagedrc.js
+++ config/lintstaged/.lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': ['npm run lint:eslint', 'npm run lint:prettier'],
-  '*.vue': ['npm run lint:prettier', 'npm run lint:stylelint'],
+  '*.vue': ['npm run lint:prettier'],
   '*.{css,scss,less,styl,html}': [
     'npm run lint:prettier',
     'npm run lint:stylelint',
