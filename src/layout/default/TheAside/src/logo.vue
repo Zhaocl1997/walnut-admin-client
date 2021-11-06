d204c10125719f2f91a1934a1746123c3194487e
zhaocl1997 <674351624@qq.com>
2021-11-06 17:54:57 +0800

    fix: logo style error

--- src/layout/default/TheAside/src/logo.vue
+++ src/layout/default/TheAside/src/logo.vue
@@ -4,13 +4,16 @@
     @click="onGoIndex"
     :style="{
       height: settings.ForDevelopers.header.height + 'px',
-      width: settings.ForDevelopers.menu.width + 'px',
+      width:
+        (app.collapse
+          ? settings.ForDevelopers.menu.collapsedWidth
+          : settings.ForDevelopers.menu.width) + 'px',
     }"
   >
     <div class="*hstack justify-evenly items-center">
       <img src="/assets/logo.png" alt="Walnut Admin Logo" class="h-9 w-9 m-1" />
 
-      <w-transition name="scale">
+      <w-transition name="slide-up">
         <div
           v-show="!app.collapse"
           class="app-text text-xl not-italic font-bold text-center"
