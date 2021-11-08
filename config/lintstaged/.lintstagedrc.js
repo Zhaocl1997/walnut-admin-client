1e07512c30c083f745e582f2030051ef4c9b8a3d
zhaocl1997 <674351624@qq.com>
2021-11-08 15:59:28 +0800

    chore: remove stylelint for html

--- config/lintstaged/.lintstagedrc.js
+++ config/lintstaged/.lintstagedrc.js
@@ -1,8 +1,5 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': ['npm run lint:eslint', 'npm run lint:prettier'],
   '*.vue': ['npm run lint:prettier'],
-  '*.{css,scss,less,styl,html}': [
-    'npm run lint:prettier',
-    'npm run lint:stylelint',
-  ],
+  '*.{css,scss,less,styl}': ['npm run lint:prettier', 'npm run lint:stylelint'],
 }
