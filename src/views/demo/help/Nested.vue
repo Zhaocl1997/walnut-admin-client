c619781be6b0b71ad786597460a135decba89b78
zhaocl1997 <674351624@qq.com>
2021-11-09 15:15:46 +0800

    wip: nested page demo

---
+++ src/views/demo/help/Nested.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Nested Menu</div>
+</template>
+
+<script lang="ts" setup>
+  const state = ref()
+</script>
+
+<script lang="ts">
+  export default defineComponent({
+    name: 'Nested',
+  })
+</script>
