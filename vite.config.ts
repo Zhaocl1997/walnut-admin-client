7edc52fb30d5e32f88d013c4f7a7e5e52cc6df02
zhaocl1997 <674351624@qq.com>
2021-11-09 15:15:14 +0800

    chore: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,17 @@ import { loadEnv } from 'vite'
 
 import { createViteProxy } from './build/vite/proxy'
 import { createVitePlugins } from './build/vite/plugin'
+import { outDir, publicDir } from './build/constant'
 
 function pathResolve(dir: string) {
   return resolve(__dirname, '.', dir)
 }
 
-const getServerHost = (env: ImportMetaEnv) => {
-  const host = env.VITE_PROXY_HOST
-  return host.includes('http') ? host.slice(7) : ''
-}
-
 // https://vitejs.dev/config/
 export default ({ command, mode }: ConfigEnv): UserConfig => {
-  const envPath = 'env'
+  const envDir = 'env'
 
-  const env = loadEnv(mode, pathResolve(envPath)) as ImportMetaEnv
+  const env = loadEnv(mode, pathResolve(envDir)) as ImportMetaEnv
 
   const alias = {
     '/@': pathResolve('src'),
@@ -30,24 +26,26 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
   return {
     base: env.VITE_PUBLIC_PATH,
 
-    envDir: envPath,
+    envDir,
+
+    publicDir,
 
     define: {
-      __VUE_I18N_LEGACY_API__: false,
       __VUE_I18N_FULL_INSTALL__: false,
+      __VUE_I18N_LEGACY_API__: false,
       __INTLIFY_PROD_DEVTOOLS__: false,
     },
 
     plugins: createVitePlugins(mode, env),
 
     resolve: {
-      alias: alias,
+      alias,
     },
 
     css: {},
 
     server: {
-      host: getServerHost(env),
+      host: env.VITE_HOST,
 
       port: env.VITE_PORT,
 
@@ -61,16 +59,56 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
     },
 
     build: {
-      brotliSize: false,
+      target: 'es2015',
+      outDir,
+      reportCompressedSize: false,
+
+      chunkSizeWarningLimit: 600,
+
       sourcemap: mode === 'staging',
+
+      // https://terser.org/docs/api-reference#minify-options
+      terserOptions: {
+        compress: {
+          keep_infinity: true,
+          drop_console: mode === 'production',
+        },
+      },
+
       rollupOptions: {
         output: {
-          // For 2.1.5 has `Maximum call stack size exceeded` error. See below.
-          // https://github.com/vitejs/vite/issues/2906
+          assetFileNames: '[ext]/[name].[hash].[ext]',
+          chunkFileNames: 'js/[name].[hash].js',
+          entryFileNames: 'js/[name].[hash].js',
+
           // https://rollupjs.org/guide/en/#outputmanualchunks
           manualChunks(id) {
             if (id.includes('node_modules')) {
+              if (id.includes('naive-ui')) {
+                return 'naive-ui'
+              }
+
+              if (id.includes('easy-fns-ts')) {
+                return 'easy-fns-ts'
+              }
+
+              if (id.includes('lodash-es')) {
+                return 'lodash-es'
+              }
+
+              if (id.includes('date-fns')) {
+                return 'date-fns'
+              }
+
+              if (id.includes('crypto-js')) {
+                return 'crypto-js'
+              }
+
               return 'vendor'
+            }
+
+            if (id.includes('src/')) {
+              return 'src'
             }
           },
         },
