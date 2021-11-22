af5f5475740f20d6f3bb9d31ef74be4e426802bd
zhaocl1997 <674351624@qq.com>
2021-11-22 15:32:20 +0800

    refactor: axios error message

--- src/utils/axios/checkStatus.ts
+++ src/utils/axios/checkStatus.ts
@@ -1,18 +1,20 @@
 import { userActionSignOut } from '/@/store/actions/user'
 
-let notiInst: any = undefined
+let notiInst: Pick<ReturnType<typeof useAppNotification>, 'error'>
 
 const responseError = (msg: string) => {
   if (notiInst) {
     return
   }
 
-  notiInst = useAppNotification().error({ title: msg, duration: 10000 })
+  // @ts-ignore
+  notiInst = useAppNotification().error({
+    title: AppI18n.global.t(msg),
+    duration: 10000,
+  })
 }
 
 export const checkReponseErrorStatus = (status?: number, msg?: string) => {
-  const { t } = AppI18n.global
-
   switch (status) {
     case 400:
       responseError(`${msg}`)
@@ -20,53 +22,53 @@ export const checkReponseErrorStatus = (status?: number, msg?: string) => {
 
     // 401: unauthorized
     case 401:
-      responseError(t('sys:ajax:401'))
+      responseError('app:ajax:401')
       userActionSignOut()
       break
 
     case 403:
-      responseError(t('sys:ajax:403'))
+      responseError('app:ajax:403')
       break
 
     // 404 not found
     case 404:
-      responseError(t('sys:ajax:404'))
+      responseError('app:ajax:404')
       break
 
     case 405:
-      responseError(t('sys:ajax:405'))
+      responseError('app:ajax:405')
       break
 
     case 408:
-      responseError(t('sys:ajax:408'))
+      responseError('app:ajax:408')
       break
 
     case 500:
-      responseError(t('sys:ajax:500'))
+      responseError('app:ajax:500')
       break
 
     case 501:
-      responseError(t('sys:ajax:501'))
+      responseError('app:ajax:501')
       break
 
     case 502:
-      responseError(t('sys:ajax:502'))
+      responseError('app:ajax:502')
       break
 
     case 503:
-      responseError(t('sys:ajax:503'))
+      responseError('app:ajax:503')
       break
 
     case 504:
-      responseError(t('sys:ajax:504'))
+      responseError('app:ajax:504')
       break
 
     case 505:
-      responseError(t('sys:ajax:505'))
+      responseError('app:ajax:505')
       break
 
     default:
-      responseError(t('sys:ajax:502'))
+      responseError('app:ajax:502')
       break
   }
 }
