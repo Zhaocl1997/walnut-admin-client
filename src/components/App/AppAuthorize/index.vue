a82b0d26cd450c78ca5dcee7eb2c890695d9c67e
zhaocl1997 <674351624@qq.com>
2021-12-09 16:17:00 +0800

    wip: authorize component

---
+++ src/components/App/AppAuthorize/index.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { renderSlot } from 'vue'
+
+  export default defineComponent({
+    name: 'AppAuthorize',
+
+    props: {
+      value: String as PropType<string>,
+    },
+
+    setup(props, { slots }) {
+      const { menu } = useAppState()
+      const permissions = menu.value.permissions
+
+      const render = () => {
+        if (permissions.includes(props.value!)) {
+          return renderSlot(slots, 'default')
+        }
+        return null
+      }
+
+      return () => render()
+    },
+  })
+</script>
