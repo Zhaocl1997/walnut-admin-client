21ffe31e8c3eeb189b0acbc933fa2022d9caede3
zhaocl1997 <674351624@qq.com>
2022-02-06 17:06:28 +0800

    refactor: auto lock only work in document visible

--- src/hooks/core/useAppAutoLock.ts
+++ src/hooks/core/useAppAutoLock.ts
@@ -1,10 +1,12 @@
 export const useAppAutoLock = () => {
   const { app, settings } = useAppState()
   const { idle } = useIdle(settings.value.ForDevelopers.app.idleMS)
+  const visibility = useDocumentVisibility()
 
   // only work in auto mode
   watchEffect(() => {
     if (
+      visibility.value === 'visible' &&
       app.value.lockMode !== AppLockModeConst.MANUAL &&
       settings.value.ForDevelopers.app.idleMS !== 0
     ) {
