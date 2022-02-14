06b088d85333346f615af453d1070b12b695e64a
zhaocl97 <zhaocl97@foxmail.com>
2022-02-14 16:53:04 +0800

    refactor: extract tab style

---
+++ src/layout/default/TheTab/src/hooks/useTabStyle.ts
@@ -0,0 +1,30 @@
+export const useTabStyle = (tabSettings: AppSettingsTab) => {
+  const { currentRoute } = useAppRouter()
+
+  const getTabStyle = (item: AppTab) => ({
+    'text-primary hover:text-primaryHover':
+      currentRoute.value.name === item.name,
+    /* card */
+    'border border-gray-400 hover:(border-primaryHover text-primaryHover)':
+      tabSettings.styleMode === TabStyleModeConst.CARD,
+    'border-primary':
+      currentRoute.value.name === item.name &&
+      tabSettings.styleMode === TabStyleModeConst.CARD,
+
+    /* flex */
+    'bg-green-50 bg-opacity-5 hover:(bg-green-100 bg-opacity-10)':
+      tabSettings.styleMode === TabStyleModeConst.FLEX,
+    'bg-green-200 bg-opacity-10 border-b-2 border-primary':
+      currentRoute.value.name === item.name &&
+      tabSettings.styleMode === TabStyleModeConst.FLEX,
+
+    /* round */
+    'bg-green-50 bg-opacity-5 hover:(bg-green-100 bg-opacity-10) rounded-t-xl':
+      tabSettings.styleMode === TabStyleModeConst.ROUND,
+    'bg-green-200 bg-opacity-10 border-l-1 border-primary':
+      currentRoute.value.name === item.name &&
+      tabSettings.styleMode === TabStyleModeConst.ROUND,
+  })
+
+  return { getTabStyle }
+}
