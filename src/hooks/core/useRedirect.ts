4a3e499bdda0e4a8e8a52aabd65f68b7eed581fa
zhaocl1997 <674351624@qq.com>
2022-02-23 21:28:35 +0800

    refactor: redirect should replace

--- src/hooks/core/useRedirect.ts
+++ src/hooks/core/useRedirect.ts
@@ -14,6 +14,7 @@ export const useRedirect = () => {
         path: AppRedirectPath + currentRoute.value.fullPath,
         query,
         params,
+        replace: true,
       }).then(() => resolve(true))
     })
   }
