5e3acb118259a61ad095b68f68bd90400b5ff5c0
zhaocl1997 <674351624@qq.com>
2022-03-06 22:21:00 +0800

    refactor: tree demo

--- src/views/demo/UI/Tree.vue
+++ src/views/demo/UI/Tree.vue
@@ -1,20 +1,278 @@
 <template>
   <w-demo-card title="Tree">
-    <w-title prefix="bar">Basic Tree </w-title>
+    <w-JSON :value="state" height="300px"></w-JSON>
 
-    <w-tree @hook="register"></w-tree>
+    <n-list>
+      <n-list-item>
+        <n-grid :cols="2">
+          <n-gi>
+            <w-title prefix="bar">Single </w-title>
+
+            <w-tree v-model:value="state.tree1" @hook="register1"></w-tree>
+          </n-gi>
+
+          <n-gi>
+            <w-title prefix="bar">Single Feedback </w-title>
+
+            <w-tree v-model:value="state.tree2" @hook="register2"></w-tree>
+          </n-gi>
+        </n-grid>
+      </n-list-item>
+
+      <n-list-item>
+        <n-grid :cols="2">
+          <n-gi>
+            <w-title
+              prefix="bar"
+              help-message="Parent and children nodes are independent"
+              >Multiple (normal)
+            </w-title>
+
+            <w-tree v-model:value="state.tree3" @hook="register3"></w-tree>
+          </n-gi>
+
+          <n-gi>
+            <w-title prefix="bar">Multiple Feedback (normal)</w-title>
+
+            <w-tree v-model:value="state.tree4" @hook="register4"></w-tree>
+          </n-gi>
+        </n-grid>
+      </n-list-item>
+
+      <n-list-item>
+        <n-grid :cols="2">
+          <n-gi>
+            <w-title
+              prefix="bar"
+              help-message="Parent and children nodes are connected \n But Half checked do not emit as v-model:value"
+              >Multiple (cascade)
+            </w-title>
+
+            <w-tree v-model:value="state.tree5" @hook="register5"></w-tree>
+          </n-gi>
+
+          <n-gi>
+            <w-title prefix="bar">Multiple Feedback (cascade)</w-title>
+
+            <w-tree v-model:value="state.tree6" @hook="register6"></w-tree>
+          </n-gi>
+        </n-grid>
+      </n-list-item>
+
+      <n-list-item>
+        <n-grid :cols="2">
+          <n-gi>
+            <w-title prefix="bar">Preset Toolbar</w-title>
+
+            <w-tree v-model:value="state.tree7" @hook="register7"></w-tree>
+          </n-gi>
+
+          <n-gi>
+            <w-title prefix="bar">Preset Toolbar (with multiple)</w-title>
+
+            <w-tree v-model:value="state.tree8" @hook="register8"></w-tree>
+          </n-gi>
+        </n-grid>
+      </n-list-item>
+
+      <n-list-item>
+        <n-grid :cols="2">
+          <n-gi>
+            <w-title prefix="bar">Draggable (with icon)</w-title>
+
+            <w-tree v-model:value="state.tree9" @hook="register9"></w-tree>
+          </n-gi>
+
+          <n-gi>
+            <w-title prefix="bar">Delete (with icon)</w-title>
+
+            <w-tree v-model:value="state.tree10" @hook="register10"></w-tree>
+          </n-gi>
+        </n-grid>
+      </n-list-item>
+
+      <n-list-item>
+        <n-grid :cols="2">
+          <n-gi>
+            <w-title prefix="bar">Preset prefix icon (with icon field)</w-title>
+
+            <w-tree v-model:value="state.tree11" @hook="register11"></w-tree>
+          </n-gi>
+
+          <n-gi>
+            <w-title prefix="bar">Context menu</w-title>
+
+            <w-tree v-model:value="state.tree12" @hook="register12"></w-tree>
+          </n-gi>
+        </n-grid>
+      </n-list-item>
+    </n-list>
   </w-demo-card>
 </template>
 
 <script lang="ts" setup>
   import { getTreeData } from '../data'
-  import { useTree } from '/@/components/UI/Tree/src/hook/useTree'
+  import { useTree } from '/@/components/UI/Tree'
 
-  const [register] = useTree({
+  const state = ref({
+    tree1: undefined,
+    tree2: [1],
+    tree3: undefined,
+    tree4: [1, 2, 3],
+    tree5: undefined,
+    tree6: [9, 13],
+    tree7: undefined,
+    tree8: undefined,
+    tree9: undefined,
+    tree10: undefined,
+    tree11: undefined,
+    tree12: undefined,
+  })
+
+  const [register1] = useTree({
     treeProps: {
       data: getTreeData(),
       labelField: '_label',
       keyField: '_id',
+      blockLine: true,
+      blockNode: true,
+    },
+  })
+
+  const [register2] = useTree({
+    treeProps: {
+      data: getTreeData(),
+      labelField: '_label',
+      keyField: '_id',
+      blockLine: true,
+      blockNode: true,
+    },
+  })
+
+  const [register3] = useTree({
+    multiple: true,
+    treeProps: {
+      data: getTreeData(),
+      labelField: '_label',
+      keyField: '_id',
+      blockLine: true,
+      blockNode: true,
+    },
+  })
+
+  const [register4] = useTree({
+    multiple: true,
+    treeProps: {
+      data: getTreeData(),
+      labelField: '_label',
+      keyField: '_id',
+      blockLine: true,
+      blockNode: true,
+    },
+  })
+
+  const [register5] = useTree({
+    multiple: true,
+    treeProps: {
+      data: getTreeData(),
+      labelField: '_label',
+      keyField: '_id',
+      blockLine: true,
+      blockNode: true,
+      cascade: true,
+    },
+  })
+
+  const [register6] = useTree({
+    multiple: true,
+    treeProps: {
+      data: getTreeData(),
+      labelField: '_label',
+      keyField: '_id',
+      blockLine: true,
+      blockNode: true,
+      cascade: true,
+    },
+  })
+
+  const [register7] = useTree({
+    toolbar: true,
+    treeProps: {
+      data: getTreeData(),
+      labelField: '_label',
+      keyField: '_id',
+      blockLine: true,
+      blockNode: true,
+      cascade: true,
+    },
+  })
+
+  const [register8] = useTree({
+    toolbar: true,
+    multiple: true,
+    treeProps: {
+      data: getTreeData(),
+      labelField: '_label',
+      keyField: '_id',
+      blockLine: true,
+      blockNode: true,
+    },
+  })
+
+  const [register9] = useTree({
+    treeProps: {
+      data: getTreeData(),
+      labelField: '_label',
+      keyField: '_id',
+      blockLine: true,
+      blockNode: true,
+      cascade: true,
+      draggable: true,
+      onDrop: ({ node, dragNode, dropPosition }) => {
+        console.log('dragged', node, dragNode, dropPosition)
+      },
+    },
+  })
+
+  const [register10] = useTree({
+    deletable: true,
+    onDelete({ option }) {
+      console.log('deleted', option)
+    },
+    treeProps: {
+      data: getTreeData(),
+      labelField: '_label',
+      keyField: '_id',
+      blockLine: true,
+      blockNode: true,
+      cascade: true,
+    },
+  })
+
+  const [register11] = useTree({
+    presetPrefixIcon: true,
+    treeProps: {
+      data: getTreeData(),
+      labelField: '_label',
+      keyField: '_id',
+      blockLine: true,
+      blockNode: true,
+      cascade: true,
+    },
+  })
+
+  const [register12] = useTree({
+    presetContextMenu: true,
+    onPaste: (copy, current) => {
+      console.log(copy, current)
+    },
+    treeProps: {
+      data: getTreeData(),
+      labelField: '_label',
+      keyField: '_id',
+      blockLine: true,
+      blockNode: true,
+      cascade: true,
     },
   })
 </script>
--- src/views/demo/data.ts
+++ src/views/demo/data.ts
@@ -25,37 +25,45 @@ export const options = [
 export const getTreeData = (): TreeDataItem<{
   _id: number
   _label: string
+  icon: string
 }>[] => {
   return [
     {
       _id: 1,
       _label: 'Level 1',
+      icon: 'ant-design:home-outlined',
       children: [
         {
           _id: 4,
           _label: 'Level 1-1',
+          icon: 'ant-design:home-outlined',
           children: [
             {
               _id: 9,
               _label: 'Level 1-1-1',
+              icon: 'ant-design:home-outlined',
             },
             {
               _id: 10,
               _label: 'Level 1-1-2',
+              icon: 'ant-design:home-outlined',
             },
           ],
         },
         {
           _id: 11,
           _label: 'Level 1-2',
+          icon: 'ant-design:home-outlined',
           children: [
             {
               _id: 12,
               _label: 'Level 1-2-1',
+              icon: 'ant-design:home-outlined',
             },
             {
               _id: 13,
               _label: 'Level 1-2-2',
+              icon: 'ant-design:home-outlined',
               _disabled: true,
             },
           ],
@@ -65,32 +73,39 @@ export const getTreeData = (): TreeDataItem<{
     {
       _id: 2,
       _label: 'Level 2',
+      icon: 'ant-design:home-outlined',
       children: [
         {
           _id: 5,
           _label: 'Level 2-1',
+          icon: 'ant-design:home-outlined',
           children: [
             {
               _id: 14,
               _label: 'Level 2-1-1',
+              icon: 'ant-design:home-outlined',
             },
             {
               _id: 15,
               _label: 'Level 2-1-2',
+              icon: 'ant-design:home-outlined',
             },
           ],
         },
         {
           _id: 6,
           _label: 'Level 2-2',
+          icon: 'ant-design:home-outlined',
           children: [
             {
               _id: 16,
               _label: 'Level 2-2-1',
+              icon: 'ant-design:home-outlined',
             },
             {
               _id: 17,
               _label: 'Level 2-2-2',
+              icon: 'ant-design:home-outlined',
             },
           ],
         },
@@ -99,14 +114,17 @@ export const getTreeData = (): TreeDataItem<{
     {
       _id: 3,
       _label: 'Level 3',
+      icon: 'ant-design:home-outlined',
       children: [
         {
           _id: 7,
           _label: 'Level 3-1',
+          icon: 'ant-design:home-outlined',
         },
         {
           _id: 8,
           _label: 'Level 3-2',
+          icon: 'ant-design:home-outlined',
         },
       ],
     },
