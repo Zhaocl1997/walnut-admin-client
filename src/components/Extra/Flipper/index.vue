09a6e904edba0fcebbbce87100a526167864749b
zhaocl1997 <674351624@qq.com>
2022-04-11 21:28:05 +0800

    refactor: extract common regex

--- src/components/Extra/Flipper/index.vue
+++ src/components/Extra/Flipper/index.vue
@@ -17,24 +17,7 @@
 </template>
 
 <script lang="ts">
-  /**
-   * Curries a predicate function to check if value matches pattern.
-   * @param {RegExp} pattern
-   * @returns {(value: string) => boolean}
-   */
-  const isPattern = (pattern: RegExp) => (value: string) => pattern.test(value)
-
-  /**
-   * Check if value is a CSS Length.
-   */
-  const isCSSLength = isPattern(
-    /^\+?(\d*\.)?\d+(em|ex|ch|rem|vh|vw|vmin|vmax|px|mm|cm|in|pt|pc|%)$/i
-  )
-
-  /**
-   * Check if value is a CSS Time.
-   */
-  const isCSSTime = isPattern(/^\+?(\d*\.)?\d+(ms|s)$/i)
+  import { isCSSLength, isCSSTime } from '/@/utils/regex'
 
   export default defineComponent({
     name: 'WFlipper',
---
+++ src/utils/regex.ts
@@ -0,0 +1,19 @@
+/**
+ * Curries a predicate function to check if value matches pattern.
+ * @param {RegExp} pattern
+ * @returns {(value: string) => boolean}
+ */
+const isPattern = (pattern: RegExp) => (value: string) => pattern.test(value)
+
+const phoneRegex =
+  /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-79])|(?:5[0-35-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[189]))\d{8}$/
+
+const CSSLengthRegex =
+  /^\+?(\d*\.)?\d+(em|ex|ch|rem|vh|vw|vmin|vmax|px|mm|cm|in|pt|pc|%)$/i
+const CSSTimeRegex = /^\+?(\d*\.)?\d+(ms|s)$/i
+
+export const isPhoneNumber = isPattern(phoneRegex)
+
+export const isCSSLength = isPattern(CSSLengthRegex)
+
+export const isCSSTime = isPattern(CSSTimeRegex)
