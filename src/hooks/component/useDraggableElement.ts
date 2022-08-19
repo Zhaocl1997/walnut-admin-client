b433aa56823457d87ae4f957c29a09df421bd71c
zhaocl97 <zhaocl97@foxmail.com>
2022-08-19 09:20:39 +0800

    feat(hook): useDraggableElement

---
+++ src/hooks/component/useDraggableElement.ts
@@ -0,0 +1,27 @@
+export const useDraggableElement = () => {
+  const el = ref<Nullable<HTMLElement>>(null)
+  const myX = ref()
+  const myY = ref()
+
+  const { width, height } = useWindowSize()
+
+  const { style, x, y } = useDraggable(el, {
+    initialValue: { x: width.value - 100, y: height.value - 100 },
+    preventDefault: true,
+    stopPropagation: true,
+    onMove: (p) => {
+      myX.value = p.x
+      myY.value = p.y
+    },
+  })
+
+  watchEffect(() => {
+    x.value = myX.value ?? width.value - 100
+    y.value = myY.value ?? height.value - 100
+  })
+
+  return {
+    el,
+    style,
+  }
+}
