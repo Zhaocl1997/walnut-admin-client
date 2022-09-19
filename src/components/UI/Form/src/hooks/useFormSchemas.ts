286c0aecc07c2b0c71497613de7087897293fa14
zhaocl97 <zhaocl97@foxmail.com>
2022-09-19 15:23:56 +0800

    revert: form schema

--- src/components/UI/Form/src/hooks/useFormSchemas.ts
+++ src/components/UI/Form/src/hooks/useFormSchemas.ts
@@ -1,4 +1,5 @@
 import type { WForm } from '../types'
+import { getFormBooleanField } from '../utils'
 
 export const useFormSchemas = (props: ComputedRef<WForm.Props>) => {
   const formSchemas = ref<WForm.Schema.Item<any>[]>([])
@@ -7,10 +8,17 @@ export const useFormSchemas = (props: ComputedRef<WForm.Props>) => {
     () => [props.value.schemas, props.value.model],
     ([s, m]) => {
       // TODO form/table _internalShow redesign
-      formSchemas.value = s?.map((i) => ({
-        ...i,
-        _internalShow: getBoolean(i._internalShow),
-      }))
+      formSchemas.value = s
+        ?.map((i) => ({
+          ...i,
+          _internalShow: getBoolean(i._internalShow),
+        }))
+        .filter(
+          // @ts-ignore
+          (i) =>
+            getFormBooleanField(i, props.value, 'vIf') &&
+            getFormBooleanField(i, props.value, 'vShow')
+        )
     },
     {
       deep: true,
