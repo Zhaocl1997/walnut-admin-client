6e8ed51787cf658316a0aa00c68682b227445a1f
Zhao <674351624@qq.com>
2022-10-31 17:23:43 +0800

    chore(deploy): use deploy-cli-service

--- build/publish/config.ts
+++
@@ -1,9 +0,0 @@
-// Here can config your cloud server relative informations
-export const publishConfig = {
-  // user name you login in the cloud server
-  cloudServerHostName: 'ubuntu',
-  // the IP of your cloud server
-  cloudServerIPAddress: '152.136.25.175',
-  // the final location of your bundle files in the cloud server
-  cloudServerClientFolderPath: '/home/ubuntu/client',
-}
