40c348247bb03a79b8b2ca84ff8c2cf578b99646
Zhaocl1997 <zhaocl97@foxmail.com>
2022-11-22 21:06:31 +0800

    refactor(useConfirm): extend options

--- src/hooks/component/useConfirm.ts
+++ src/hooks/component/useConfirm.ts
@@ -1,3 +1,5 @@
+import type { DialogOptions } from 'naive-ui'
+
 export const useAppDialog = () => {
   return window.$dialog
 }
@@ -5,7 +7,7 @@ export const useAppDialog = () => {
 /**
  * @description some cases need to ask for user confirm to continue next action
  */
-export const useAppConfirm = (msg: string) => {
+export const useAppConfirm = (msg: string, options?: DialogOptions) => {
   const dialog = useAppDialog()
 
   return new Promise<boolean>((res) => {
@@ -15,6 +17,7 @@ export const useAppConfirm = (msg: string) => {
       negativeText: AppI18n.global?.t('app.button.no'),
       positiveText: AppI18n.global?.t('app.button.yes'),
       autoFocus: false,
+      ...options,
       onPositiveClick: () => {
         res(true)
       },
