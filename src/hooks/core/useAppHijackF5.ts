51876463bbcedc44bf5cb416cf69039defd7dc94
Zhaocl1997 <zhaocl97@foxmail.com>
2022-12-11 19:51:57 +0800

    perf(hook): hijack f5 hook optimise

--- src/hooks/core/useAppHijackF5.ts
+++ src/hooks/core/useAppHijackF5.ts
@@ -1,21 +1,25 @@
-let cleanup: Fn
+import type { EffectScope } from 'vue'
 
 export const useAppHijackF5 = () => {
+  let scope: EffectScope
+
   const appSetting = useAppStoreSetting()
 
   watch(
     () => appSetting.app.hijackRefresh,
     (v) => {
       if (v) {
-        cleanup = useEventListener('keydown', async (e) => {
-          if (e.key === 'F5') {
-            e.preventDefault()
-            await useRedirect()
-          }
+        scope = effectScope()
+        scope.run(() => {
+          useEventListener('keydown', async (e) => {
+            if (e.key === 'F5') {
+              e.preventDefault()
+              await useRedirect()
+            }
+          })
         })
       } else {
-        cleanup()
-        cleanup = () => {}
+        scope?.stop()
       }
     },
     {
