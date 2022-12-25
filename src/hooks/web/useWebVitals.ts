ce53ac931581dd89ef21186e847b677c64208416
Zhaocl1997 <zhaocl97@foxmail.com>
2022-12-25 20:05:12 +0800

    wip: web-vitals

---
+++ src/hooks/web/useWebVitals.ts
@@ -0,0 +1,29 @@
+import { onCLS, onFCP, onFID, onINP, onLCP, onTTFB } from 'web-vitals/attribution'
+
+function logDelta({ name, delta, value, id, attribution }) {
+  const eventParams = {
+    // Built-in params:
+    value: delta, // Use `delta` so the value can be summed.
+    // Custom params:
+    metric_id: id, // Needed to aggregate events.
+    metric_value: value, // Optional.
+    metric_delta: delta, // Optional.
+  }
+
+  // console.log(eventParams);
+}
+
+export const reportWebVitals = () => {
+  // Cumulative Layout Shift 累积布局偏移
+  onCLS(logDelta, { reportAllChanges: true })
+  // First Input Delay 首次输入延迟
+  onFID(logDelta, { reportAllChanges: true })
+  // Largest Contentful Paint 最大内容绘制
+  onLCP(logDelta, { reportAllChanges: true })
+  // First Contentful Paint 首次内容绘制
+  onFCP(logDelta, { reportAllChanges: true })
+  // Interaction to Next Paint
+  onINP(logDelta, { reportAllChanges: true })
+  // Time to First Byte 第一字节时间
+  onTTFB(logDelta, { reportAllChanges: true })
+}
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import 'uno.css'
 import '@unocss/reset/normalize.css'
 import 'animate.css'
 import './assets/styles/main.scss'
-
 import { App, setupApp } from './App'
+import { reportWebVitals } from '@/hooks/web/useWebVitals'
+
 (async () => {
   const app = createApp(App)
 
   await setupApp(app)
 
   app.mount('#app')
+
+  reportWebVitals()
 })()
