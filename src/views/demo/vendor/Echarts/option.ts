eb917e7b237ec82cee419a05d012bcf6075cc06a
Zhaocl1997 <zhaocl97@foxmail.com>
2022-12-28 10:51:56 +0800

    refactor: rename folders

---
+++ src/views/demo/vendor/Echarts/option.ts
@@ -0,0 +1,179 @@
+export const option1: EChartsOption = {
+  title: {
+    text: 'Stacked Area Chart',
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      label: {
+        backgroundColor: '#6a7985',
+      },
+    },
+  },
+  legend: {
+    data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine'],
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {},
+    },
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true,
+  },
+  xAxis: [
+    {
+      type: 'category',
+      boundaryGap: false,
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+  ],
+  yAxis: [
+    {
+      type: 'value',
+    },
+  ],
+  series: [
+    {
+      name: 'Email',
+      type: 'line',
+      stack: 'Total',
+      areaStyle: {},
+      emphasis: {
+        focus: 'series',
+      },
+      data: [120, 132, 101, 134, 90, 230, 210],
+    },
+    {
+      name: 'Union Ads',
+      type: 'line',
+      stack: 'Total',
+      areaStyle: {},
+      emphasis: {
+        focus: 'series',
+      },
+      data: [220, 182, 191, 234, 290, 330, 310],
+    },
+    {
+      name: 'Video Ads',
+      type: 'line',
+      stack: 'Total',
+      areaStyle: {},
+      emphasis: {
+        focus: 'series',
+      },
+      data: [150, 232, 201, 154, 190, 330, 410],
+    },
+    {
+      name: 'Direct',
+      type: 'line',
+      stack: 'Total',
+      areaStyle: {},
+      emphasis: {
+        focus: 'series',
+      },
+      data: [320, 332, 301, 334, 390, 330, 320],
+    },
+    {
+      name: 'Search Engine',
+      type: 'line',
+      stack: 'Total',
+      label: {
+        show: true,
+        position: 'top',
+      },
+      areaStyle: {},
+      emphasis: {
+        focus: 'series',
+      },
+      data: [820, 932, 901, 934, 1290, 1330, 1320],
+    },
+  ],
+}
+
+export const option2: EChartsOption = {
+  title: {
+    text: 'World Population',
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow',
+    },
+  },
+  legend: {},
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true,
+  },
+  xAxis: {
+    type: 'value',
+    boundaryGap: [0, 0.01],
+  },
+  yAxis: {
+    type: 'category',
+    data: ['Brazil', 'Indonesia', 'USA', 'India', 'China', 'World'],
+  },
+  series: [
+    {
+      name: '2011',
+      type: 'bar',
+      data: [18203, 23489, 29034, 104970, 131744, 630230],
+    },
+    {
+      name: '2012',
+      type: 'bar',
+      data: [19325, 23438, 31000, 121594, 134141, 681807],
+    },
+  ],
+}
+
+export const option3: EChartsOption = {
+  tooltip: {
+    trigger: 'item',
+  },
+  legend: {
+    top: '5%',
+    left: 'center',
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: ['40%', '70%'],
+      avoidLabelOverlap: false,
+      itemStyle: {
+        borderRadius: 10,
+        borderColor: '#fff',
+        borderWidth: 2,
+      },
+      label: {
+        show: false,
+        position: 'center',
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: '40',
+          fontWeight: 'bold',
+        },
+      },
+      labelLine: {
+        show: false,
+      },
+      data: [
+        { value: 1048, name: 'Search Engine' },
+        { value: 735, name: 'Direct' },
+        { value: 580, name: 'Email' },
+        { value: 484, name: 'Union Ads' },
+        { value: 300, name: 'Video Ads' },
+      ],
+    },
+  ],
+}
---
+++ src/views/demo/vendor/Tinymce/index.vue
@@ -0,0 +1,69 @@
+<script lang="ts" setup>
+const value = ref(
+  '<p style="text-align: center;"><span style="color: rgb(224, 62, 45); font-size: 36pt;"><em><strong>This is default content</strong></em></span></p>',
+)
+
+const formData = ref({
+  title: 'This is default title',
+  content:
+      '<p style="text-align: center;"><span style="color: rgb(224, 62, 45); font-size: 36pt;"><em><strong>This is default content</strong></em></span></p>',
+})
+
+const [register] = useForm<typeof formData.value>({
+  labelWidth: 100,
+  schemas: [
+    {
+      type: 'Base:Input',
+      formProp: {
+        path: 'title',
+        label: 'Title',
+        required: true,
+      },
+    },
+    {
+      type: 'Vendor:Tinymce',
+      formProp: {
+        path: 'content',
+        label: 'Content',
+        required: true,
+      },
+    },
+  ],
+})
+</script>
+
+<script lang="ts">
+export default defineComponent({
+  name: 'TinymceDemo',
+})
+</script>
+
+<template>
+  <w-demo-card title="Tinymce Demo">
+    <n-list>
+      <n-list-item>
+        <w-title prefix="bar">
+          Basic usage
+        </w-title>
+
+        <pre class="whitespace-pre-wrap">Value: {{ value }}</pre>
+        <w-tinymce
+          v-model:value="value"
+          disabled
+        />
+      </n-list-item>
+
+      <n-list-item>
+        <w-title prefix="bar">
+          Form usage
+        </w-title>
+
+        <pre class="whitespace-pre-wrap">{{ formData }}</pre>
+
+        <w-form :model="formData" @hook="register" />
+      </n-list-item>
+    </n-list>
+  </w-demo-card>
+</template>
+
+<style lang="scss" scoped></style>
