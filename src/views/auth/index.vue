c754b697610739a8cb21380d4f0b3f06002d2c54
Zhaocl1997 <zhaocl97@foxmail.com>
2023-01-10 21:35:23 +0800

    wip: untyper

--- src/views/auth/index.vue
+++ src/views/auth/index.vue
@@ -1,8 +1,9 @@
 <script lang="tsx" setup>
+import { UnTyper } from 'untyper'
 import AuthFormTab from './src/index.vue'
 import { useDemonstrate } from './useDemonstrate'
 
-const { t } = useAppI18n()
+const { t, locale } = useAppI18n()
 const { title: envTitle } = useAppEnv('title')
 
 const { signInRef } = useDemonstrate()
@@ -10,6 +11,19 @@ const { signInRef } = useDemonstrate()
 const onClickBeian = () => {
   openExternalLink('http://beian.miit.gov.cn/')
 }
+
+watch(locale, () => {
+  nextTick(async () => {
+    // const title1 = document.getElementById('untyper1')!
+    // const unTyper1 = new UnTyper(title1, { speed: 250, startDelay: 250 })
+    // await unTyper1.type(t('app.auth.h1'), { delay: 200 }).go()
+
+    const title2 = document.getElementById('untyper2')!
+    title2.innerText = ''
+    const unTyper2 = new UnTyper(title2, { speed: 200, startDelay: 250 })
+    await unTyper2.type(t('app.auth.h2')).go()
+  })
+}, { immediate: true })
 </script>
 
 <script lang="tsx">
@@ -23,9 +37,7 @@ export default defineComponent({
 <template>
   <!-- https://tailwindcomponents.com/component/login-register-form-with-image -->
   <section class="min-h-screen flex items-stretch">
-    <div
-      class="hstack space-x-4 absolute top-8 right-8 z-50 children:cursor-pointer"
-    >
+    <div class="hstack space-x-4 absolute top-8 right-8 z-50 children:cursor-pointer">
       <n-button text>
         <WAppLocalePicker />
       </n-button>
@@ -40,18 +52,14 @@ export default defineComponent({
     >
       <div class="absolute bg-black opacity-60 inset-0 z-0" />
       <div class="w-full px-24 z-10">
-        <h1 class="text-5xl font-bold text-left tracking-wide text-gray-50">
+        <h1 id="untyper1" class="text-5xl font-bold text-left tracking-wide text-gray-50">
           {{ t('app.auth.h1') }}
         </h1>
-        <p class="text-3xl my-4 text-gray-50">
-          {{ t('app.auth.h2') }}
-        </p>
+        <p id="untyper2" class="text-3xl my-4 text-gray-50" />
       </div>
     </div>
 
-    <div
-      class="lg:w-1/2 w-full flex items-center justify-center text-center bg-bodyColor relative"
-    >
+    <div class="lg:w-1/2 w-full flex items-center justify-center text-center bg-bodyColor relative">
       <div>
         <div
           class="absolute lg:hidden z-10 inset-0 bg-no-repeat bg-cover items-center"
@@ -60,21 +68,11 @@ export default defineComponent({
           <div class="absolute bg-black opacity-60 inset-0 z-0" />
         </div>
 
-        <n-card
-          style="z-index: 100"
-          hoverable
-          class="rounded-3xl relative light:shadow-2xl"
-        >
+        <n-card style="z-index: 100" hoverable class="rounded-3xl relative light:shadow-2xl">
           <div class="vstack items-center justify-center">
             <h1 class="hstack items-center justify-center mb-4">
-              <img
-                src="/assets/logo.png"
-                :alt="envTitle"
-                class="w-16 sm:w-12 md:w-16 lg:w-16 xl:w-16 2xl:w-16"
-              >
-              <span
-                class="md:text-2xl lg:text-2xl xl:text-3xl 2xl:text-3xl ml-4 whitespace-nowrap"
-              >
+              <img src="/assets/logo.png" :alt="envTitle" class="w-16 sm:w-12 md:w-16 lg:w-16 xl:w-16 2xl:w-16">
+              <span class="md:text-2xl lg:text-2xl xl:text-3xl 2xl:text-3xl ml-4 whitespace-nowrap">
                 {{ envTitle }}
               </span>
             </h1>
@@ -87,10 +85,7 @@ export default defineComponent({
           </div>
         </n-card>
 
-        <n-text
-          class="absolute bottom-4 right-1/2 translate-x-1/2 cursor-pointer"
-          @click="onClickBeian"
-        >
+        <n-text class="absolute bottom-4 right-1/2 translate-x-1/2 cursor-pointer" @click="onClickBeian">
           黑ICP备2022002207号
         </n-text>
       </div>
