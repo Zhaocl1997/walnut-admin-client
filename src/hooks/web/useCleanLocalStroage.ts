57f2d0a3e53650c63dcc0f7c6a28f33c9dfbd059
Zhaocl1997 <zhaocl97@foxmail.com>
2023-01-10 21:16:45 +0800

    fix(storage): only clean storage that key start with prefix

--- src/hooks/web/useCleanLocalStroage.ts
+++ src/hooks/web/useCleanLocalStroage.ts
@@ -1,11 +1,9 @@
-import pkg from '../../../package.json'
-
 export const useCleanLocalStroage = () => {
+  const { version } = __APP_INFO__
   Object.keys(window.localStorage).forEach((key) => {
-    if (!key.includes(pkg.version)) {
-      // remove localeStorage that are not current version
-      // also that are not set by us, which key does not have version flag
+    // only clean prefixed storage
+    // version not matched
+    if (key.startsWith(storagePrefix) && !key.includes(version))
       window.localStorage.removeItem(key)
-    }
   })
 }
