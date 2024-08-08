f7e395192f252b76a99fd58fe62644299a87bb48
Zhaocl1997 <674351624@qq.com>
2024-08-08 13:41:42 +0800

    fix(axios): adapter error

--- src/utils/axios/config.ts
+++ src/utils/axios/config.ts
@@ -6,15 +6,15 @@ const { axiosTimeout } = useAppEnv('seconds')
 
 const { httpUrl } = useAppEnv('proxy')
 
+const adapter = axios.getAdapter('fetch')
+
 export const originalConfig: AxiosRequestConfig = {
   baseURL: httpUrl,
 
   // time out, default is 10s
   timeout: Number(axiosTimeout) * 1000,
 
-  adapter: retryAdapterEnhancer(throttleAdapterEnhancer(
-    cacheAdapterEnhancer(axios.defaults.adapter!),
-  )),
+  adapter: [retryAdapterEnhancer(adapter), throttleAdapterEnhancer(adapter), cacheAdapterEnhancer(adapter)],
 
   // default transform "true"/"false" to true/false
   _transformStringBoolean: true,
