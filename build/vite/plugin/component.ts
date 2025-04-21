761fbad20448cd0ddc03f53712cb2615c320b9c6
Zhaocl1997 <674351624@qq.com>
2025-04-21 15:38:03 +0800

    chore: sass new syntax

--- build/vite/plugin/component.ts
+++ build/vite/plugin/component.ts
@@ -7,8 +7,6 @@ function WalnutAdminComponentResolver(): ComponentResolver {
   const allComponents = fg.sync('src/components/**/**/index.ts', { dot: true })
   const componentMap = Object.fromEntries(allComponents.filter(i => !i.includes('utils')).map(i => [i.split('/').slice(-2, -1)[0], i.replace('src', '@')]))
 
-  console.log('Auto Register Components', componentMap)
-
   return {
     type: 'component',
     resolve: (name) => {
