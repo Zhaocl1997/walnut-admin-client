ced26fd36ad28d38a8ae9af3c068a3d2e9daf475
Zhaocl1997 <674351624@qq.com>
2025-04-23 13:37:09 +0800

    chore: app console log optimise

--- src/utils/log/combine.ts
+++ src/utils/log/combine.ts
@@ -6,18 +6,18 @@ const upperName = name.toUpperCase()
 
 export function AppConsoleLog(module: string, msg: string | unknown) {
   // eslint-disable-next-line no-console
-  console.log(`%c[${upperName}][${getNow()}][${module}]\n${msg}`, 'color: #63e2b7; font-weight: bold; font-size: 16px')
+  console.log(`%c[${upperName}][${getNow()}][${module}]`, 'color: #63e2b7; font-weight: bold; font-size: 12px', msg)
 }
 
 export function AppConsoleInfo(module: string, msg: string | unknown) {
   // eslint-disable-next-line no-console
-  console.info(`%c[${upperName}][${getNow()}][${module}]\n${msg}`, 'color: #63e2b7; font-weight: bold; font-size: 16px')
+  console.info(`%c[${upperName}][${getNow()}][${module}]`, 'color: #63e2b7; font-weight: bold; font-size: 12px', msg)
 }
 
 export function AppConsoleWarn(module: string, msg: string | unknown) {
-  console.warn(`%c${upperName}][${getNow()}][${module}]\n${msg}`, 'color: #70c0e8; font-weight: bold; font-size: 16px')
+  console.warn(`%c${upperName}][${getNow()}][${module}]`, 'color: #70c0e8; font-weight: bold; font-size: 12px', msg)
 }
 
 export function AppConsoleErr(module: string, msg: string | unknown) {
-  console.error(`%c[${upperName}][${getNow()}][${module}]\n${msg}`, 'color: #e88080; font-weight: bold; font-size: 16px')
+  console.error(`%c[${upperName}][${getNow()}][${module}]`, 'color: #e88080; font-weight: bold; font-size: 12px', msg)
 }
