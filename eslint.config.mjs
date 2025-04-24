d72994bf46f73af410ba0a2058f422a3420ba1c9
Zhaocl1997 <674351624@qq.com>
2025-04-24 08:50:45 +0800

    chore(eslint): allow namespace

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,9 @@ import antfu from '@antfu/eslint-config'
 export default antfu(
   {
     unocss: true,
+    rules: {
+      // LINK reasonable https://stackoverflow.com/a/78566802
+      'ts/no-namespace': 'off',
+    },
   },
 )
