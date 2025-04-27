ecdb6c8d733be92ca2ff12cdde092bcde7e15fa2
Zhaocl1997 <674351624@qq.com>
2025-04-27 09:33:57 +0800

    refactor: transition group demo

--- src/views/demo/Extra/Transition.vue
+++ src/views/demo/Extra/Transition.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { random } from 'lodash-es'
+
 defineOptions({
   name: 'TransitionDemo',
   defaultView: false,
@@ -34,16 +36,49 @@ const [register] = useForm({
     },
   ],
 })
+
+const nums = ref([1, 2, 3])
+const items = computed(() => nums.value.filter(i => i % 2 === 0))
+
+function onAddRandom() {
+  nums.value.push(random(3, 100))
+}
 </script>
 
 <template>
-  <WDemoCard title="Transition & Transition Select" class="h-72">
-    <WForm :model="formData" @hook="register" />
+  <WDemoCard title="Transition & Transition Select">
+    <n-list>
+      <n-list-item>
+        <WTitle prefix="bar" class="mb-2">
+          Transition
+        </WTitle>
 
-    <div class="h-36">
-      <WTransition :name="formData.name">
-        <div v-show="show" class="h-36 w-36 bg-red-400" />
-      </WTransition>
-    </div>
+        <WForm :model="formData" @hook="register" />
+
+        <div class="h-36">
+          <WTransition :name="formData.name">
+            <div v-show="show" class="h-36 w-36 bg-red-400" />
+          </WTransition>
+        </div>
+      </n-list-item>
+
+      <n-list-item>
+        <WTitle prefix="bar" class="mb-2">
+          Transition Group
+        </WTitle>
+
+        <n-button class="mb-2" @click="onAddRandom">
+          Add Random
+        </n-button>
+
+        <div class="flex flex-row flex-wrap gap-2">
+          <WTransition :name="formData.name" group>
+            <div v-for="(item, index) in items" :key="index" class="h-36 w-36 flex items-center justify-center bg-red-400 text-center text-16 font-bold">
+              {{ item }}
+            </div>
+          </WTransition>
+        </div>
+      </n-list-item>
+    </n-list>
   </WDemoCard>
 </template>
