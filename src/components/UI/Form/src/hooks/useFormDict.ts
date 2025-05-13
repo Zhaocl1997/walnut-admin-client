ce362559774ba65f531e6dec0b97749f251a6512
Zhaocl1997 <674351624@qq.com>
2025-05-13 15:47:12 +0800

    refactor(form): use form dict

--- src/components/UI/Form/src/hooks/useFormDict.ts
+++ src/components/UI/Form/src/hooks/useFormDict.ts
@@ -2,10 +2,10 @@ import type { WForm } from '../types'
 
 export function useFormDict<T>(schemas: WForm.Schema.Item<T>[]) {
   if (schemas.some(i => i.type === 'Business:Dict')) {
-    initDict(
-      schemas
-        .filter(i => i.type === 'Business:Dict')
-        .map(i => (i as WForm.Schema.SchemaItem.DictSchema<T>).componentProp?.dictType) as string[],
-    )
+    const dictTypes = schemas
+      .filter(i => i.type === 'Business:Dict')
+      .map(i => (i as WForm.Schema.SchemaItem.DictSchema<T>).componentProp?.dictType)
+
+    initDict(dictTypes)
   }
 }
