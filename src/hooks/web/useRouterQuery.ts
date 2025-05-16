24d6e671db17f6d9e604c03decef5e15bbfc61fd
Zhaocl1997 <674351624@qq.com>
2025-05-16 08:54:29 +0800

    fix: useRouterQuery missing query

--- src/hooks/web/useRouterQuery.ts
+++ src/hooks/web/useRouterQuery.ts
@@ -1,13 +1,13 @@
 export function useRouterQuery(path: string) {
-  const router = useAppRouter()
   const route = useAppRoute()
+  const router = useAppRouter()
 
   const routeQuery = computed({
     get() {
       return route.query[path] as string ?? undefined
     },
     set(val) {
-      router.replace({ query: { [path]: val } })
+      router.replace({ query: { ...router.currentRoute.value.query, [path]: val } })
     },
   })
 
---
+++ src/views/demo/Extra/Query.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+defineOptions({
+  name: 'QueryDemo',
+  defaultView: false,
+})
+
+const q1 = useRouterQuery('q1')
+const q2 = useRouterQuery('q2')
+const q3 = useRouterQuery('q3')
+</script>
+
+<template>
+  <WDemoCard title="Arrow">
+    <n-list>
+      <n-list-item>
+        <WTitle prefix="bar" class="mb-2">
+          Dynamic Query
+        </WTitle>
+
+        <n-space>
+          <n-input v-model:value="q1" />
+          <n-input v-model:value="q2" />
+          <n-input v-model:value="q3" />
+        </n-space>
+      </n-list-item>
+    </n-list>
+  </WDemoCard>
+</template>
