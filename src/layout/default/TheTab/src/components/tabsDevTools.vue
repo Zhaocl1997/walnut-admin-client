ce78961fd41619a8ac7d94e804acf3385c624c9f
Zhaocl1997 <674351624@qq.com>
2025-05-16 11:36:49 +0800

    refactor: dev tool text desc

--- src/layout/default/TheTab/src/components/tabsDevTools.vue
+++ src/layout/default/TheTab/src/components/tabsDevTools.vue
@@ -11,11 +11,25 @@ const { currentMouseTab, devToolShow, onOpenFile } = getTabsContext()
 <template>
   <n-modal v-if="getShowDevTools" v-model:show="devToolShow">
     <n-card style="width: 600px" role="dialog" aria-modal="true">
-      <WJSON :value="currentMouseTab" height="auto" />
+      <WJSON :value="currentMouseTab" height="300px" />
 
-      <n-button v-show="!currentMouseTab?.meta.url" @click="onOpenFile">
-        Open vue file related to this tab in
-        <span class="text-red-500">&ensp;VS Code</span>
+      <n-button v-show="!currentMouseTab?.meta.url" class="mt-4" @click="onOpenFile">
+        <div class="hstack items-center gap-x-2">
+          <WMessage
+            :msg="[
+              'Notice now you have multiple ways to open current file in development environment',
+              '1: (best) use vue devtool, the function is built in',
+              '2: (nice choice) use turbo console to open the file, the logic behind should be the same as vue devtool, but you need a console in the target file',
+              '3: (unnecessary) use vite plugin(i cannot remember the name), which is duplicated with vue devtools function so this project did not install',
+            ]"
+          />
+
+          <div>
+            Open file in
+            <span class="text-red-500">VS Code</span>
+            (Not recommended)
+          </div>
+        </div>
       </n-button>
     </n-card>
   </n-modal>
