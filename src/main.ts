15951234560b857b1b44fb26f7f661c1849b1f64
Zhaocl1997 <674351624@qq.com>
2025-05-22 14:37:53 +0800

    chore: css reset for unocss

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,8 @@ import { App, setupApp } from './App'
 // unocss
 import 'virtual:uno.css'
 
-// reset css
-// TODO influence naive ui style
-import '@unocss/reset/sanitize/sanitize.css'
-import '@unocss/reset/sanitize/assets.css'
+// LINK https://github.com/unocss/unocss/issues/2127
+import '@unocss/reset/tailwind-compat.css'
 
 // LINK https://utc.yuy1n.io/features/highlight.html#options-1
 import '~console/theme-detect'
