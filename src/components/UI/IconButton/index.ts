f39f1a0dd71c86652aaa521851d647c55de6a9a2
Zhaocl1997 <674351624@qq.com>
2025-06-04 14:19:03 +0800

    fix: icon button props error

--- src/components/UI/IconButton/index.ts
+++ src/components/UI/IconButton/index.ts
@@ -1,10 +1,11 @@
 import type { PopconfirmProps, TooltipProps } from 'naive-ui'
+import type { AllowedComponentProps } from 'vue'
 import type { ICompUIButtonProps } from '../Button'
 import type { ICompUIIconProps } from '../Icon'
 
 export { default } from './index.vue'
 
-export interface ICompUIIconButtonProps {
+export interface ICompUIIconButtonProps extends AllowedComponentProps {
   iconProps?: ICompUIIconProps
   buttonProps?: ICompUIButtonProps
   tooltip?: boolean
