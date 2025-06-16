457c89abd4ee7f7e9e91a33894be961fb46593d8
Zhaocl1997 <674351624@qq.com>
2025-06-16 20:18:50 +0800

    chore: remove useless generate

--- build/generate/genPaths.ts
+++ build/generate/genPaths.ts
@@ -5,16 +5,12 @@ import { BuildUtilsWriteFile, generatedPathsFilePath } from '../utils'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-;
-
-(async function () {
+;(async function () {
   await BuildUtilsWriteFile(
     generatedPathsFilePath,
     `// This file is generated, please do not change.
   export const paths = {
-    root: ${JSON.stringify(__dirname.replace('build\\generate', ''))},
     view: ${JSON.stringify(__dirname.replace('build\\generate', 'src\\views'))},
-    build: ${JSON.stringify(__dirname.replace('\\generate', ''))},
   }
   `,
   )
