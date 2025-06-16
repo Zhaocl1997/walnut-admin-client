8d53166256a99df8405a8aa3fcaac991773f8a60
Zhaocl1997 <674351624@qq.com>
2025-06-16 16:28:08 +0800

    refactor: remove useless

--- build/utils/paths.ts
+++ build/utils/paths.ts
@@ -1,12 +1,8 @@
-export const stagingBuildLogPath = 'report/stage.log'
 export const typesCheckLogPath = 'report/tsc.log'
 export const bundleSizeStatsLogPath = 'report/stats.html'
 
 // paths.ts
 export const generatedPathsFilePath = 'build/_generated/paths.ts'
-
-// publish.bat
-export const generatedPublishBatFileName = 'build/_generated/publish.bat'
 
 // app settings json schema file path
 export const AppSettingsJSONSchemaFilePath = '.vscode/appSettings.schema.json'
--- src/const/links.ts
+++ src/const/links.ts
@@ -1,5 +1,3 @@
 export const EXTERNAL_LINKS = {
-  UA: 'https://ip.useragentinfo.com/json',
-  IP: 'https://ip2c.org/s',
   GEOIP: 'https://api.ip.sb/geoip',
 } as const
