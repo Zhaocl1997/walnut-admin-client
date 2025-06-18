8b4168a68389170a4addc01435c8701ab5a5e303
Zhao <674351624@qq.com>
2025-06-18 13:32:56 +0800

    Delete src/components/Vendor/Echarts directory

--- src/components/Vendor/Echarts/index.ts
+++
@@ -1,9 +0,0 @@
-import './on-demand'
-
-export default createAsyncComponent(() => import('./index.vue'))
-
-export interface ICompVendorEChartsProps {
-  option: EChartsOption
-  height?: string
-  width?: string
-}
