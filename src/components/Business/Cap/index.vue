12d4fc7403bd4d8ba5a6d4d84462ded06e640b57
Zhaocl1997 <674351624@qq.com>
2025-06-25 12:49:20 +0800

    feat: cap support i18n and dark & cap demo

--- src/components/Business/Cap/index.vue
+++ src/components/Business/Cap/index.vue
@@ -12,7 +12,10 @@ function onCapSolve(e: { detail: { token: string } }) {
   const token = e.detail.token
 
   appCapJSToken.onCapSuccess!(token)
-  appCapJSToken.onCloseCapModal()
+  const id = setTimeout(() => {
+    appCapJSToken.onCloseCapModal()
+    clearTimeout(id)
+  }, 1000)
 }
 </script>
 
@@ -24,10 +27,32 @@ function onCapSolve(e: { detail: { token: string } }) {
     :mask-closable="false"
     :default-button="false"
     :fullscreen="false"
-    display-directive="show"
     :title="$t('app.base.cap')"
     width="330px"
   >
-    <cap-widget id="walnut-admin-cap" :data-cap-api-endpoint="authCapApiEndpoint" @solve="onCapSolve" />
+    <n-element>
+      <div
+        style="
+    --cap-background: var(--body-color);
+    --cap-color: var(--text-color-1);
+    --cap-border-color: var(--n-border-color);
+    --cap-checkbox-border-radius: 6px;
+    --cap-border-radius: var(--n-border-radius);
+    --cap-checkbox-background: transparent;
+    --cap-checkmark: url(data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2224%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%3E%3Cstyle%3E%40keyframes%20anim%7B0%25%7Bstroke-dashoffset%3A23.21320343017578px%7Dto%7Bstroke-dashoffset%3A0%7D%7D%3C%2Fstyle%3E%3Cpath%20fill%3D%22none%22%20stroke%3D%22%23fff%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%20stroke-width%3D%222%22%20d%3D%22m5%2012%205%205L20%207%22%20style%3D%22stroke-dashoffset%3A0%3Bstroke-dasharray%3A23.21320343017578px%3Banimation%3Aanim%20.5s%20ease%22%2F%3E%3C%2Fsvg%3E);
+    --cap-spinner-color: var(--n-color);
+    --cap-spinner-background-color: var(--n-text-color)"
+      >
+        <cap-widget
+          id="walnut-admin-cap"
+          :data-cap-api-endpoint="authCapApiEndpoint"
+          :data-cap-i18n-verifying-label="$t('comp.cap.verifying')"
+          :data-cap-i18n-initial-state="$t('comp.cap.initial')"
+          :data-cap-i18n-solved-label="$t('comp.cap.solved')"
+          :data-cap-i18n-error-label="$t('comp.cap.error')"
+          @solve="onCapSolve"
+        />
+      </div>
+    </n-element>
   </WModal>
 </template>
---
+++ src/views/demo/business/Cap.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+defineOptions({
+  name: 'CapDemo',
+  defaultView: false,
+})
+
+const appCapJSToken = useAppStoreCapJSToken()
+
+async function onNormalMode() {
+  await appCapJSToken.onOpenCapModal(async (token) => {
+    useAppMsgSuccess(token)
+  })
+}
+
+async function onInvisible() {
+  const token = await appCapJSToken.refreshCapJSToken()
+  useAppMsgSuccess(token)
+}
+</script>
+
+<template>
+  <WDemoCard title="Cap">
+    <n-button class="mb-2" @click="onNormalMode">
+      Cap Validate
+    </n-button>
+    <br>
+    <n-button @click="onInvisible">
+      Cap Invisible Validate
+    </n-button>
+  </WDemoCard>
+</template>
