b168681e3be075ef4ee2e414f9b2f3ff0cb65885
Zhaocl1997 <674351624@qq.com>
2025-07-17 13:20:41 +0800

    fix: geo ip initial error

--- src/hooks/web/useExternalGeoIP.ts
+++ src/hooks/web/useExternalGeoIP.ts
@@ -1,10 +1,9 @@
 import axios from 'axios'
-import { isEmpty } from 'lodash-es'
 
 export const GeoIPInfo = useAppStorage<Partial<ExternalGeoIPInfo>>(AppConstPersistKey.GEO_IP_INFO, {}, { usePresetKey: false, expire: Number.POSITIVE_INFINITY })
 
 export async function useExternalGeoIP() {
-  if (!isEmpty(GeoIPInfo.value))
+  if (Object.keys(GeoIPInfo.value).length)
     return
 
   const res = await axios.get<ExternalGeoIPInfo>(EXTERNAL_LINKS.GEOIP)
